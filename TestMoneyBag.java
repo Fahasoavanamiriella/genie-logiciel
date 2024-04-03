741f759772de7c0f01962268da46ec49bbaba1a5
Fahasoavanamiriella <165952076+Fahasoavanamiriella@users.noreply.github.com>
2024-04-03 22:22:25 +0200

    Add files via upload

---
+++ TestMoneyBag.java
@@ -0,0 +1,14 @@
+package fr.emse.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestMoneyBag {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
